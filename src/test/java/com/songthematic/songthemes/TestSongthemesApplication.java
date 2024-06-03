dced41353de20dff83c5d2b4fec7aa63e095f37a
Gandhinath Swaminathan <62220739+minimalistinnovator@users.noreply.github.com>
2024-06-03 15:31:16 -0700

    Initial commit

---
+++ src/test/java/com/songthematic/songthemes/TestSongthemesApplication.java
@@ -0,0 +1,23 @@
+package com.songthematic.songthemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSongthemesApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(SongthemesApplication::main).with(TestSongthemesApplication.class).run(args);
+	}
+
+}
